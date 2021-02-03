5fdf3aa70960dd007afe241466c5dae043753574
ikakooo <ikakooo@ikakooos-MacBook-Pro.local>
2021-02-03 06:28:42 -0800

    ikakooo

---
+++ untitledApp/untitledAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  untitledAppApp.swift
+//  untitledApp
+//
+//  Created by ikakooo on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct untitledAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
